3d2613018524b78732c0eac1037bcb7649f83eb7
YummYume <givsoul@gmail.com>
2020-12-03 14:33:19 +0100

    Ajout jeu d'essai

---
+++ jeu_essai_script.sql
@@ -0,0 +1,5 @@
+INSERT INTO dbo.UTILISATEUR (Login_utilisateur, Mdp_utilisateur, Droit_utilisateur)
+VALUES ('me', 'me', 'utilisateur'),
+('test', 'test', 'admin'),
+('Yam', 'Lythis', 'admin'),
+('Morgan', 'Lythis', 'compta')
